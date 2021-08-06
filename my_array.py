f5d86f8de777438a5e413249c4077a56d952cd9a
Claudio Vazquez <claudiojvv10@gmail.com>
2021-08-05 20:26:01 -0500

    Add grid

--- my_array.py
+++ my_array.py
@@ -22,6 +22,7 @@ class Array(object):
             fill_value (any, optional): value at each position. Defaults to None.
         """
         self.items = list()
+        self.n = 0
         for i in range(capacity):
             self.items.append(fill_value)
 
@@ -44,3 +45,12 @@ class Array(object):
     def __setitem__(self, index, new_item):
         """Subscript operator for replacement at index."""
         self.items[index] = new_item
+
+    def __next__(self):
+        """Replace and return the values of the array with a random number using __next__"""
+        if self.n <= self.__len__() - 1:
+            self.__setitem__(self.n, randint(1, 100))
+            self.n += 1
+            return self.items
+        else:
+            raise StopIteration
